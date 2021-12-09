8395e85ca504fffb808445ee2ede1fca173c6a4d
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-12-10 01:00:16 +0200

    groups, disciplines tables

--- src/components/Forms/AddDisciplineForm.js
+++ src/components/Forms/AddDisciplineForm.js
@@ -1,38 +1,46 @@
-import {useState, useRef, useContext, useEffect} from 'react';
-import AuthContext from '../../store/auth-context';
-import classes from './AddGroup.module.css';
+import {useState, useEffect} from 'react';
+import classes from '../Auth/AuthForm.module.css';
 
 const AddDisciplineForm = () => {
 
     const [name, setName] = useState('');
     const [lectors,setLectors] = useState([]);
-    const [lecturerId,setLecturerId] = useState(1);
-
-    const authCtx = useContext(AuthContext);
+    const [lecturerId, setLecturerId] = useState(1);
+    const [disciplines, setDisciplines] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-
 
     const getLectors = async () => {
         const data = await fetch("/lectors/all");
         const dataJson = await data.json();
         setLectors(dataJson);
     }
+    const getDisciplines = async () => {
+        const data = await fetch("/disciplines");
+        const dataJson = await data.json();
+        setDisciplines(dataJson);
+    }
+
+    const deleteDiscipline = async (id) => {
+        const response = await fetch("/disciplines/"+id, {
+            method: 'DELETE',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        response.ok ?  alert("Дисципліна успішно видалена.") :  alert("Дисципліну не було видалено. Status: "+ response.status);
+    }
 
     useEffect(  () => {
         getLectors();
+        getDisciplines();
     }, [])
 
 
     const submitHandler = async (event) => {
         event.preventDefault();
-        console.log(name);
-        console.log(lectors);
-        console.log(lecturerId)
-
-        // to - do : Add validation
 
         setIsLoading(true);
-
 
         const response = await fetch("/disciplines", {
             method: 'POST',
@@ -46,7 +54,6 @@ const AddDisciplineForm = () => {
             },
         });
 
-        console.log(response);
         response.ok ?  alert("Дисципліна успішно додано.") :  alert("Дисципліну не було додано. Status: "+ response.status);
         setName('');
         setLecturerId(1);
@@ -54,34 +61,50 @@ const AddDisciplineForm = () => {
     };
 
     return (
-        <section className={classes.auth}>
-            <h1>Додай нову дисципліну тут</h1>
-            <form onSubmit={submitHandler}>
+        <div className={classes.wrapper}>
+            <section className={classes.auth}>
+                <h1>Додай нову дисципліну тут</h1>
+                <form onSubmit={submitHandler}>
 
-                <div className="form-group">
-                    <textarea type="text" name="name" id="name" placeholder="Назва" className="form-control" onChange={e => setName(e.target.value)}/>
-                </div>
+                    <div className="form-group">
+                        <textarea type="text" name="name" id="name" placeholder="Назва" className="form-control" onChange={e => setName(e.target.value)}/>
+                    </div>
 
-                <div className={classes.control}>
-                    <label htmlFor='lector'>Лектор</label>
-                    <select name="lector" id="lector" onChange={e => setLecturerId(e.target.value)}>
-                        {(lectors || []).map(lector => (
-                            <option value={lector.lecturerId} key={lector.lecturerId}>
-                                {lector.surname + ' ' + lector.name + ' '+ lector.middlename}
-                            </option>
-                        ))}
-                    </select>
-                </div>
+                    <div className={classes.control}>
+                        <label htmlFor='lector'>Лектор</label>
+                        <select name="lector" id="lector" onChange={e => setLecturerId(e.target.value)}>
+                            {(lectors || []).map(lector => (
+                                <option value={lector.lecturerId} key={lector.lecturerId}>
+                                    {lector.surname + ' ' + lector.name + ' '+ lector.middlename}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
 
-                <div className={classes.actions}>
-                    {!isLoading && (
-                        <button>Створити групу</button>
-                    )}
-                    {isLoading && <p>Відправляємо дані...</p>}
+                    <div className={classes.actions}>
+                        {!isLoading && (
+                            <button>Створити групу</button>
+                        )}
+                        {isLoading && <p>Відправляємо дані...</p>}
 
-                </div>
-            </form>
-        </section>
+                    </div>
+                </form>
+            </section>
+            <section className={classes.tableWrapper}>
+                <table className={classes.disciplineTable}>
+                    <tr>
+                        <th>Предмет</th>
+                        <th>Лектор</th>
+                    </tr>
+                    {(disciplines || []).map(i => (
+                        <tr key={i.disciplineId}>
+                            <td>{i.name}</td>
+                            <td>{lectors.filter(l => l.lecturerId === i.lecturerId).map(l => (l.surname + ' ' + l.name + ' '+ l.middlename))}</td>
+                        </tr>
+                    ))}
+                </table>
+            </section>
+        </div>
     );
 };
 
