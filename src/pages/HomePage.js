e21e352eb141f141c03feda2e9721b1007123489
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-12-10 03:33:14 +0200

    text added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const HomePage = () => {
 
-    return <div></div>;
+    return (<div><h1>Вітаємо на сервісі для перегляду та редагування розкладу факультету інформатики!</h1></div>);
 };
 
 export default HomePage;
